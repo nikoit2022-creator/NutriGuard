3d4cbe7b15c2973040ce67fb9369bb47d7c9e355
nikoit2022-creator <nikoit2022@gmail.com>
2026-08-28 14:03:26 +0300

    fix(android): use default debug signing

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -48,12 +48,6 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
   }
 
   buildTypes {
@@ -63,7 +57,6 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
