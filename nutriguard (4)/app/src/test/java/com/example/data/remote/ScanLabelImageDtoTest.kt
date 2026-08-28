ec36348d2a3fba757673c637d7b7be61c4309a0c
nikoit2022-creator <nikoit2022@gmail.com>
2026-08-28 10:59:41 +0300

    test(android): run scan DTO tests with Robolectric

--- nutriguard (4)/app/src/test/java/com/example/data/remote/ScanLabelImageDtoTest.kt
+++ nutriguard (4)/app/src/test/java/com/example/data/remote/ScanLabelImageDtoTest.kt
@@ -9,7 +9,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class ScanLabelImageDtoTest {
 
     private val fullResponseJson = """
