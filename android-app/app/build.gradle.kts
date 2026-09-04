f6ad228c2405bbeadb3cffd0837f5c7f01b9355f
nikoit2022-creator <nikoit2022@gmail.com>
2026-09-04 12:47:47 +0300

    Bump Android app version to 1.1

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     applicationId = "com.aistudio.nutriguard.analyzer"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1"
 
     buildConfigField("String", "BACKEND_BASE_URL", "\"$escapedBackendBaseUrl\"")
 
